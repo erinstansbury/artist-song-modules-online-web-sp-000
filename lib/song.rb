bb74dc52403f468fa8e0c94978339a95c9f1fb7a
Erin Stansbury <ekstansbury@gmail.com>
2020-03-26 18:14:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,13 +6,16 @@ class Song
   attr_reader :artist
 
   @@songs = []
+  def initialize
+    @@songs << self
+  end
 
   def artist=(artist)
     @artist = artist
   end
 
   def self.find_by_name(name)
-    @@songs.detect{|s| s.name == name}
+    @@songs.detect{|a| a.name == name}
   end
 
   def self.all
