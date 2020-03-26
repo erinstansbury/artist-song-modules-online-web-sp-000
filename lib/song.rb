c0edcc3736453193caf6cbb61ecd83e4eb0a75e5
Erin Stansbury <ekstansbury@gmail.com>
2020-03-26 17:45:22 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+class Song
+  extend Memorable::ClassMethods
+  attr_accessor :name
+  attr_reader :artist
+
+  @@songs = []
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+  def self.all
+    @@songs
+  end
+end
